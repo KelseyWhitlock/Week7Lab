85a93c4b1c45895bb1b3af6b4d864d770863dc5f
KELSEY-LAPTOP\Kelsey <k.whitlock@hotmail.ca>
2023-02-28 08:00:28 -0700

    Working on the servlet and the xml

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -6,11 +6,14 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import services.RoleService;
+import services.UserService;
 
 /**
  *
@@ -20,6 +23,17 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        UserService userS = new UserService();
+        RoleService roleS = new RoleService();
+        String touch = request.getParameter("action");
+        try{
+            if(touch != null && touch.equals("edit")){
+                String email = request.getParameter("email");
+            }
+        }catch(Exception ex){
+             Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", "error");
+        }
         
     }
 
